bf9e5d0efc6f6972830873f92cc187fbeacef74e
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-01-14 10:43:43 -0500

    add frontend files- delayed tranfers of files when copied

---
+++ frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import {useState, useEffect} from "react";
+import axios from "axios";
+import {BrowserRouter, Link, Routes, Route} from "react-router-dom";
+
+//import components
+
+//import css
+import "./App.css";
+
+function App() {
+  // FOR EXAMPLE LOGIN FUNCTIONALITY
+  // const {currentUser, setCurrentUser} = useState();
+
+  //EXAMPLE
+  // useEffect(() => {
+  //   axios.get("/api/candidates").then(res => {
+  //     setCandidates(res.data);
+  //   });
+  // }, []);
+
+  // EXAMPLE LOGIN FUNCTIONALITY
+  // const login = () => {
+  //   axios.post("/api/login").then(res => {
+  //     setCurrentUser(res.data);
+  //   });
+  // };
+
+  // TO CHECK FOR COOKIE (IF USER SIGNED IN) ON FIRST LOAD
+  // useEffect(() => {
+  //   axios.post("/api/authenticate").then(res => {
+  //     setCurrentUser(res.data);
+  //   });
+  // }, []);
+
+  return (
+    <div className="App">
+      <Link to="/">Home </Link>
+      {!currentUser && <button onClick={login}>LOG IN</button>}
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;

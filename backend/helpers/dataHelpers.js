0e35e3ff445b72a97afb272aecd897d71fe8a7d4
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-14 15:43:19 -0500

    change tab spacing

--- backend/helpers/dataHelpers.js
+++ backend/helpers/dataHelpers.js
@@ -2,23 +2,23 @@ const getProjectsByDevs = (devsProjects) => {
   const projectsByDevs = {};
 
   for (let project of devsProjects) {
-      if (!projectsByDevs[project.junior_dev_id]) {
-          projectsByDevs[project.junior_dev_id] = {
-              devId: project.junior_dev_id,
-              firstName: project.first_name,
-              lastName: project.last_name,
-              email: project.email,
-              projects: [],
-          };
-      }
+    if (!projectsByDevs[project.junior_dev_id]) {
+      projectsByDevs[project.junior_dev_id] = {
+          devId: project.junior_dev_id,
+          firstName: project.first_name,
+          lastName: project.last_name,
+          email: project.email,
+          projects: [],
+        };
+    }
 
-      projectsByDevs[project.junior_dev_id].projects.push({
-          title: project.title,
-          description: project.description,
-          thumbnailPhotoUrl: project.thumbnail_photo_url,
-          githubLink: project.github_link,
-          liveLink: project.live_link,
-      });
+    projectsByDevs[project.junior_dev_id].projects.push({
+      title: project.title,
+      description: project.description,
+      thumbnailPhotoUrl: project.thumbnail_photo_url,
+      githubLink: project.github_link,
+      liveLink: project.live_link,
+    });
 
   }
 

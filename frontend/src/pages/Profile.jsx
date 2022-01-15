6858ced56ad0637afa54c2afbd5b63afa7fb3aaf
Alex-Reyne <creativereyne@gmail.com>
2022-01-15 13:41:28 -0600

    center profile content

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -8,11 +8,15 @@ export default function Profile(props) {
 		<div className='profile-content'>
 			<section className='profile-bio'>
 				<img id='profile-pic' src='nil'></img>
-				<h1>Name: {name}</h1>
-				<h1>Email: {email}</h1>
-				<h1>GitHub: {github}</h1>
-				<h1>LinkedIn: {linkedin}</h1>
-				<h1>Bio: {bio}</h1>
+				<section>
+					<h1>Name: {name}</h1>
+					<h1>Bio: {bio}</h1>
+				</section>
+				<section>
+					<h1>Email: {email}</h1>
+					<h1>GitHub: {github}</h1>
+					<h1>LinkedIn: {linkedin}</h1>
+				</section>
 			</section>
 			<section className='cards'>
 				<Grid

bdf9ce2671973f7c1aef1613b2223da8b72adac1
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-01-15 12:51:43 -0500

    fixing login component form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,48 +1,56 @@
 import './styles/Login.scss';
-import { TextField, Button } from '@mui/material';
+import {TextField, Button} from '@mui/material';
 import axios from 'axios';
 
 export default function Login(props) {
-	// // FOR EXAMPLE LOGIN FUNCTIONALITY
-	// const {currentUser, setCurrentUser} = useState();
+  // // FOR EXAMPLE LOGIN FUNCTIONALITY
+  // const {currentUser, setCurrentUser} = useState();
 
-	// // EXAMPLE
-	// useEffect(() => {
-	//   axios.get("/api/candidates").then(res => {
-	//     setCandidates(res.data);
-	//   });
-	// }, []);
+  // // EXAMPLE
+  // useEffect(() => {
+  //   axios.get("/api/candidates").then(res => {
+  //     setCandidates(res.data);
+  //   });
+  // }, []);
 
-	// // EXAMPLE LOGIN FUNCTIONALITY
-	const login = e => {
-		e.preventDefault();
-		axios
-			.post('/api/login')
-			.then(res => {
-				// setCurrentUser(res.data);
-				console.log(res.data);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	};
+  // // EXAMPLE LOGIN FUNCTIONALITY
+  const login = e => {
+    console.log(e.target.value);
+    e.preventDefault();
 
-	// // TO CHECK FOR COOKIE (IF USER SIGNED IN) ON FIRST LOAD
-	// useEffect(() => {
-	//   axios.post("/api/authenticate").then(res => {
-	//     setCurrentUser(res.data);
-	//   });
-	// }, []);
+    axios
+      .post('/api/login', {email: '1@junior.com', password: '123'})
+      .then(res => {
+        console.log('heelo2?');
+        // setCurrentUser(res.data);
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-	return (
-		<div className='login-content'>
-			<form className='login' onSubmit={login}>
-				<TextField id='email' label='email' variant='outlined' />
-				<TextField id='password' label='password' variant='outlined' />
-				<Button variant='contained' size='large' type='submit'>
-					LOG IN
-				</Button>
-			</form>
-		</div>
-	);
+  // // TO CHECK FOR COOKIE (IF USER SIGNED IN) ON FIRST LOAD
+  // useEffect(() => {
+  //   axios.post("/api/authenticate").then(res => {
+  //     setCurrentUser(res.data);
+  //   });
+  // }, []);
+
+  return (
+    <div className="login-content">
+      <form className="login" onSubmit={login}>
+        <input id="email" name="email" label="email" variant="outlined" />
+        <input
+          id="password"
+          name="password"
+          label="password"
+          variant="outlined"
+        />
+        <button variant="contained" size="large" type="submit">
+          LOG IN
+        </button>
+      </form>
+    </div>
+  );
 }

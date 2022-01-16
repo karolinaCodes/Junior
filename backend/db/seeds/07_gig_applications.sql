67b13560ca99f4904bf656492f7b7ca5d15658c9
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-16 15:19:20 -0500

    fix seed data

--- backend/db/seeds/07_gig_applications.sql
+++ backend/db/seeds/07_gig_applications.sql
@@ -1,5 +1,8 @@
 INSERT INTO gig_applications (gig_posting_id, junior_dev_id)
 VALUES (1, 1);
+INSERT INTO gig_applications (gig_posting_id, junior_dev_id)
 VALUES (1, 2);
+INSERT INTO gig_applications (gig_posting_id, junior_dev_id)
 VALUES (2, 1);
+INSERT INTO gig_applications (gig_posting_id, junior_dev_id)
 VALUES (2, 3);

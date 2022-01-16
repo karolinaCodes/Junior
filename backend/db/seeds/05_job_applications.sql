bdaedae7949865640476d8ee35b0f0111df735f6
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-16 15:01:32 -0500

    add more job and gig applications to seed data

--- backend/db/seeds/05_job_applications.sql
+++ backend/db/seeds/05_job_applications.sql
@@ -1,2 +1,5 @@
 INSERT INTO job_applications (job_posting_id, junior_dev_id)
 VALUES (1, 1);
+VALUES (1, 2);
+VALUES (2, 1);
+VALUES (2, 3);
--- backend/db/seeds/07_gig_applications.sql
+++ backend/db/seeds/07_gig_applications.sql
@@ -1,2 +1,5 @@
 INSERT INTO gig_applications (gig_posting_id, junior_dev_id)
 VALUES (1, 1);
+VALUES (1, 2);
+VALUES (2, 1);
+VALUES (2, 3);

15c635b41d76467a753f301525d3b65986babc66
Alex-Reyne <creativereyne@gmail.com>
2022-01-16 22:04:55 -0600

    update new project schema to include original_request

--- backend/db/schema/03_projects.sql
+++ backend/db/schema/03_projects.sql
@@ -4,8 +4,9 @@ CREATE TABLE "projects" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "junior_dev_id" INTEGER REFERENCES junior_devs(id) ON DELETE CASCADE,
   "title" VARCHAR(50) NOT NULL,
-  "description" VARCHAR(225) NOT NULL,
+  "description" VARCHAR(1000) NOT NULL,
   "thumbnail_photo_url" VARCHAR(255) NOT NULL DEFAULT 'https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-Online-Code-Editors.png',
   "github_link" VARCHAR(225),
-  "live_link" VARCHAR(225)
+  "live_link" VARCHAR(225),
+  "original_request" VARCHAR(1000),
 );

faaf1350f09955ccd4ffa99d68374fc39bd535ba
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-17 21:24:03 -0500

    fix application page

--- frontend/src/components/Applications.jsx
+++ frontend/src/components/Applications.jsx
@@ -1,26 +1,32 @@
 import './styles/PortfolioCard.scss';
-import {TextField, Button, Modal, Box, Grid, Paper} from '@mui/material';
+import {Grid} from '@mui/material';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export default function ApplicationCard(props) {
 	const { first_name, last_name, email, bio, photo_url, github_url, linkedIn_url, resume_url, location } =
 		props;
-	// const datePostedFormatted = new Date(date_posted).toLocaleDateString();
 
 	return (
-		<Grid container>
+		<Grid container direction='row'>
 			<Grid item>
 				<img src={photo_url} alt={`${first_name} ${last_name}`} height="150" />
 			</Grid>
-			<Grid item>
-				<h1>{`${first_name} ${last_name}`}</h1>
-				<p>{email}</p>
-				<p>Github: {github_url}</p>
-				<p>LinkedIn: {linkedIn_url}</p>
-				<p>Resume: {resume_url}</p>
-				<p>{location}</p>
-			</Grid>
-			<Grid item>
-				<p>{bio}</p>
+			<Grid container direction='row' sx={{flex: 1}}>
+				<Grid item>
+					<h3>{`${first_name} ${last_name}`}</h3>
+				</Grid>
+				<Grid container direction='row'>
+					<Grid item>
+						<p>{email}</p>
+						<p>Github: {github_url}</p>
+						<p>LinkedIn: {linkedIn_url}</p>
+						<p>Resume: {resume_url}</p>
+						<p><LocationOnIcon /> {location}</p>
+					</Grid>
+					<Grid item>
+						<p>{bio}</p>
+					</Grid>
+				</Grid>
 			</Grid>
 		</Grid>
 	);
--- frontend/src/pages/JobApplications.jsx
+++ frontend/src/pages/JobApplications.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 import {TextField, Button, Modal, Box, Grid, Paper} from '@mui/material';
 import {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
-import Applications from '../components/Applications';
+import ApplicationCard from '../components/Applications';
 
 import WorkIcon from '@mui/icons-material/Work';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
@@ -12,9 +12,9 @@ import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 
 import axios from 'axios';
 
-export default function LandingPage(props) {
-  // const [open, setOpen] = useState(false);
-  // const {job_id} = useParams();
+export default function Applications(props) {
+  // Get the job_posting id from url
+  const {job_id} = useParams();
   
   const [profile, setProfile] = useState({
     employer: {},
@@ -24,16 +24,13 @@ export default function LandingPage(props) {
     gig: {},
     applications: []
   });
-  const [application, setApplication] = useState({
-    id: 0,
-    dev: {}
-  });
+
   const [openModal, setOpenModal] = useState(false);
   const [modalData, setModalData] = useState();
 
   const {company_name, email, photo_url, bio} = profile.employer;
   // const employer_id = profile.employer.id;
-  const {job_title, description, city, salary_min, salary_max, job_type, is_remote, date_posted, is_open} = posting.job;
+  const {employer_photo_url, job_title, description, city, salary_min, salary_max, job_type, is_remote, date_posted, is_open} = posting.job;
   // const job_id = posting.job.id;
   // const {} = application.id;
   // const {first_name, last_name, email, photo_url, github_url, linkedIn_url, bio} = application.dev;
@@ -41,30 +38,23 @@ export default function LandingPage(props) {
   // const {company_name, email, photo_url, bio} = profile.applications;
 
   // TESTING
-  const dev_id = 1;
   const employer_id = 1;
-  const job_id = 1;
   // TESTING
 
   useEffect(() => {
     const employerUrl = '/api/employers/' + employer_id;
     const jobUrl = '/api/job_postings/' + job_id;
     const applicationsUrl = '/api/job_postings/' + job_id + '/applications';
-    const devUrl = '/api/devs/' + dev_id;
     Promise.all([
       axios.get(employerUrl),
       axios.get(jobUrl),
       axios.get(applicationsUrl),
-      axios.get(devUrl),
     ]).then((all) => {
-      const [employerData, jobData, applicationsData, devData] = all;
+      const [employerData, jobData, applicationsData] = all;
       setProfile(prev => ({...prev, employer: employerData.data}));
       setPosting(prev => ({...prev,
         job: jobData.data,
         applications: applicationsData.data
-      }));
-      setApplication(prev => ({...prev,
-        dev: devData.data
       }));
     });
   }, []);
@@ -88,7 +78,7 @@ export default function LandingPage(props) {
 
   const applicationsArray = posting.applications;
 	const parsedApplications = applicationsArray.map(application => {
-    const data = (<Applications key={'Application-modal-' + application.id} {...application} />);
+    const data = (<ApplicationCard key={'Application-modal-' + application.id} {...application} />);
 		return (
       <Grid item xs={10} key={'Application-grid-' + application.id}>
         <Paper
@@ -96,7 +86,7 @@ export default function LandingPage(props) {
           setModalData(data);
           handleView();
         }} key={'Application-paper-' + application.id}>
-          <Applications key={'Application-card-' + application.id}
+          <ApplicationCard key={'Application-card-' + application.id}
             {...application}
           />
         </Paper>
@@ -106,31 +96,29 @@ export default function LandingPage(props) {
 
   return (
     <div className="job-content">
-      <h1>{posting.job.job_title}</h1>
+      <h1>{job_title}</h1>
       <div>
         <WorkIcon />
-        <span>{posting.job.is_remote ? 'Remote' : posting.job.city}</span>
+        <span>{is_remote ? 'Remote' : city}</span>
         <LocationOnIcon />
-        <span>{posting.job.job_type}</span>
+        <span>{job_type}</span>
         <LocalOfferIcon />
-        <span>{posting.job.salary_min}</span>
+        <span>{salary_min}</span>
       </div>
       <span>
         Posted{' '}
-        {new Date(posting.job.date_posted).toLocaleDateString('en-US', {
+        {new Date(date_posted).toLocaleDateString('en-US', {
           year: 'numeric',
           month: 'long',
           day: 'numeric',
         })}
       </span>
-      <img src={posting.job.photo_url} />
       <h2>Description</h2>
       <div className="job-description">
-        <p>{posting.job.description}</p>
-        <img src={posting.job.employer_photo_url} />
+        <p>{description}</p>
       </div>
       <div className="application-list">
-        <Grid container direction='column'>
+        <Grid container direction='column' spacing={3}>
           <h1>Applications</h1>
           Total applications: {posting.applications.length}
           {parsedApplications}
@@ -141,6 +129,7 @@ export default function LandingPage(props) {
         onClose={handleView}
       >
         <Box sx={style}>
+          {modalData}
         </Box>
       </Modal>
     </div>

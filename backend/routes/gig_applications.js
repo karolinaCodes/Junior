762e339b6c863e3870c209b2061f8bd07a82e75e
Alex-Reyne <creativereyne@gmail.com>
2022-01-20 22:06:21 -0600

    add db queries for accepted job/gig applications

--- backend/routes/gig_applications.js
+++ backend/routes/gig_applications.js
@@ -1,29 +1,51 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = ({getGigApplicationById, addGigApplication}) => {
-  /* GET a gig application by gig_applications.id */
-  router.get('/:id', (req, res) => {
-    getGigApplicationById(req.params.id)
-      .then(job => res.json(job))
-      .catch(err =>
-        res.json({
-          error: err.message,
-        })
-      );
-  });
+module.exports = ({ getGigApplicationById, addGigApplication }) => {
+	/* GET a gig application by gig_applications.id */
+	router.get('/:id', (req, res) => {
+		getGigApplicationById(req.params.id)
+			.then(gig => res.json(gig))
+			.catch(err =>
+				res.json({
+					error: err.message,
+				})
+			);
+	});
 
-  /* POST a new gig application */
-  router.post('/new', (req, res) => {
-    const {gig_posting_id, junior_dev_id} = req.body;
-    addGigApplication(gig_posting_id, junior_dev_id)
-      .then(newGigApplication => res.json(newGigApplication))
-      .catch(err =>
-        res.json({
-          error: err.message,
-        })
-      );
-  });
+	/* GET all accepted gig applications */
+	router.get('/accepted', (req, res) => {
+		getAcceptedGigApplications(req.params.id)
+			.then(application => res.json(application))
+			.catch(err =>
+				res.json({
+					error: err.message,
+				})
+			);
+	});
 
-  return router;
+	/* Accept a gig application */
+	router.post('/accept', (req, res) => {
+		acceptGigApplication(req.params.id)
+			.then(application => res.json(application))
+			.catch(err =>
+				res.json({
+					error: err.message,
+				})
+			);
+	});
+
+	/* POST a new gig application */
+	router.post('/new', (req, res) => {
+		const { gig_posting_id, junior_dev_id } = req.body;
+		addGigApplication(gig_posting_id, junior_dev_id)
+			.then(newGigApplication => res.json(newGigApplication))
+			.catch(err =>
+				res.json({
+					error: err.message,
+				})
+			);
+	});
+
+	return router;
 };
--- backend/routes/job_applications.js
+++ backend/routes/job_applications.js
@@ -1,29 +1,51 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = ({getJobApplicationById, addJobApplication}) => {
-  /* GET a job application by job_applications.id */
-  router.get('/:id', (req, res) => {
-    getJobApplicationById(req.params.id)
-      .then(application => res.json(application))
-      .catch(err =>
-        res.json({
-          error: err.message,
-        })
-      );
-  });
+module.exports = ({ getJobApplicationById, addJobApplication }) => {
+	/* GET a job application by job_applications.id */
+	router.get('/:id', (req, res) => {
+		getJobApplicationById(req.params.id)
+			.then(application => res.json(application))
+			.catch(err =>
+				res.json({
+					error: err.message,
+				})
+			);
+	});
 
-  /* POST a new job application */
-  router.post('/new', (req, res) => {
-    const {job_posting_id, junior_dev_id} = req.body;
-    addJobApplication(job_posting_id, junior_dev_id)
-      .then(newJobApplication => res.json(newJobApplication))
-      .catch(err =>
-        res.json({
-          error: err.message,
-        })
-      );
-  });
+	/* GET all accepted job applications */
+	router.get('/accepted', (req, res) => {
+		getAcceptedJobApplications(req.params.id)
+			.then(application => res.json(application))
+			.catch(err =>
+				res.json({
+					error: err.message,
+				})
+			);
+	});
 
-  return router;
+	/* Accept a gig application */
+	router.get('/accept', (req, res) => {
+		acceptJobApplication(req.params.id)
+			.then(application => res.json(application))
+			.catch(err =>
+				res.json({
+					error: err.message,
+				})
+			);
+	});
+
+	/* POST a new job application */
+	router.post('/new', (req, res) => {
+		const { job_posting_id, junior_dev_id } = req.body;
+		addJobApplication(job_posting_id, junior_dev_id)
+			.then(newJobApplication => res.json(newJobApplication))
+			.catch(err =>
+				res.json({
+					error: err.message,
+				})
+			);
+	});
+
+	return router;
 };

da8352e5962eb05e489540634cdeeac40861db25
Alex-Reyne <creativereyne@gmail.com>
2022-01-22 12:03:33 -0600

    add completeGigApplication query

--- backend/db/schema/10_accepted_gigs.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS accepted_gigs CASCADE;
-
-CREATE TABLE "accepted_gigs" (
-  "id" SERIAL PRIMARY KEY NOT NULL,
-  "gig_posting_id" INTEGER REFERENCES gig_postings(id) ON DELETE CASCADE,
-  "junior_dev_id" INTEGER REFERENCES junior_devs(id) ON DELETE CASCADE,
-  "employer_id" TIMESTAMP NOT NULL DEFAULT NOW(),
-  "is_complete" BOOLEAN NOT NULL DEFAULT FALSE
-);

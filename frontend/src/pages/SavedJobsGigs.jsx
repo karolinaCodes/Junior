5ebac9c68ba1d451cee8ccc4bc7523d3c8543dd4
Alex-Reyne <creativereyne@gmail.com>
2022-01-22 21:41:25 -0600

    fix infinite loop with savedjobs gigs and key prop bug

--- frontend/src/pages/SavedJobsGigs.jsx
+++ frontend/src/pages/SavedJobsGigs.jsx
@@ -1,67 +1,68 @@
 import './styles/Profile.scss';
-import {Card, Grid} from '@mui/material';
-import {useContext} from 'react';
+import axios from 'axios';
+import { Card, Grid } from '@mui/material';
+import { useContext, useEffect } from 'react';
 import SavedJobsGigsCard from '../components/SavedJobsGigsCard';
-import {UserContext} from '../Providers/userProvider';
+import { UserContext } from '../Providers/userProvider';
 
 export default function Applications(props) {
-  const {currentUser, savedJobsGigs} = useContext(UserContext);
+	const { currentUser, savedJobsGigs, setSavedJobsGigs } =
+		useContext(UserContext);
 
-  return (
-    <div className="application-content">
-      <Grid container direction="column">
-        <h1>Saved</h1>
-        <section className="application-cards">
-          <h3>Saved Jobs</h3>
-          <Grid container item>
-            {savedJobsGigs.jobs &&
-              savedJobsGigs.jobs.map((savedJob, index) => {
-                return (
-                  <Grid
-                    item
-                    xs={12}
-                    md={6}
-                    key={'SavedJobs-grid-' + savedJob.job_posting_id}
-                  >
-                    <Card
-                      className="card-click"
-                      key={'SavedJobs-paper-' + savedJob.job_posting_id}
-                    >
-                      <SavedJobsGigsCard
-                        key={'SavedJobs-card-' + savedJob.job_posting_id}
-                        saved={savedJob}
-                      />
-                    </Card>
-                  </Grid>
-                );
-              })}
-          </Grid>
-          <h3>Saved Gigs</h3>
-          <Grid container item>
-            {savedJobsGigs.gigs &&
-              savedJobsGigs.gigs.map(savedGig => {
-                return (
-                  <Grid
-                    item
-                    xs={12}
-                    md={6}
-                    key={'SavedGigs-grid-' + savedGig.gig_posting_id}
-                  >
-                    <Card
-                      className="card-click"
-                      key={'SavedGigs-paper-' + savedGig.gig_posting_id}
-                    >
-                      <SavedJobsGigsCard
-                        key={'SavedGigs-card-' + savedGig.gig_posting_id}
-                        saved={savedGig}
-                      />
-                    </Card>
-                  </Grid>
-                );
-              })}
-          </Grid>
-        </section>
-      </Grid>
-    </div>
-  );
+	useEffect(() => {
+		axios
+			.get(`/api/save/${currentUser.id}`)
+			.then(res => {
+				setSavedJobsGigs(res.data);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}, [currentUser, setSavedJobsGigs]);
+
+	return (
+		<div className='application-content'>
+			<Grid container direction='column'>
+				<h1>Saved</h1>
+				<section className='application-cards'>
+					<h3>Saved Jobs</h3>
+					<Grid container item>
+						{savedJobsGigs.jobs &&
+							savedJobsGigs.jobs.map((savedJob, index) => {
+								return (
+									<Grid
+										item
+										xs={12}
+										md={6}
+										key={'SavedJobs-grid-' + savedJob.job_posting_id}
+									>
+										<Card className='card-click'>
+											<SavedJobsGigsCard saved={savedJob} />
+										</Card>
+									</Grid>
+								);
+							})}
+					</Grid>
+					<h3>Saved Gigs</h3>
+					<Grid container item>
+						{savedJobsGigs.gigs &&
+							savedJobsGigs.gigs.map(savedGig => {
+								return (
+									<Grid
+										item
+										xs={12}
+										md={6}
+										key={'SavedGigs-grid-' + savedGig.gig_posting_id}
+									>
+										<Card className='card-click'>
+											<SavedJobsGigsCard saved={savedGig} />
+										</Card>
+									</Grid>
+								);
+							})}
+					</Grid>
+				</section>
+			</Grid>
+		</div>
+	);
 }

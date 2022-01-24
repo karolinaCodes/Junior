7164a25bd1f2eba691d881c6fbc69b12df05a2e0
Alex-Reyne <creativereyne@gmail.com>
2022-01-24 01:31:15 -0600

    create component out of user Projects

--- backend/db/seeds/03_projects.sql
+++ backend/db/seeds/03_projects.sql
@@ -16,3 +16,28 @@ VALUES (1, 'Tinyapp',
 'https://raw.githubusercontent.com/karolinaCodes/Tinyapp/master/docs/urls_register.jpeg', 
 'https://github.com/karolinaCodes/Tinyapp', 'https://github.com/karolinaCodes/Tinyapp', 'Original Request test');
 
+INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
+VALUES (2, 'Creative Reyne',
+'A simple URL shortener app that takes a regular URL and transforms it into an encoded version (similar to bit.ly).', 
+'https://raw.githubusercontent.com/Alex-Reyne/CreativeReyne/master/unknown.png', 
+'https://github.com/Alex-Reyne/CreativeReyne', 'https://www.creativereyne.ca/', 'Original Request test');
+
+INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
+VALUES (2, 'Tweeter',
+'Tweeter is a simple, single-page Twitter clone with the functionality of posting tweets and viewing them on the main tweeter page.', 
+'https://raw.githubusercontent.com/Alex-Reyne/tweeter/master/docs/Screen%20Shot%202021-11-18%20at%208.42.44%20PM.png', 
+'https://github.com/Alex-Reyne/tweeter', 'https://github.com/Alex-Reyne/tweeter', 'Original Request test');
+
+INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
+VALUES (2, 'Interview Scheduler',
+'A single-page React application that allows students of the LightHouse Labs Web Development Bootcamp to book and manage an interview with a mentor.', 
+'https://raw.githubusercontent.com/Alex-Reyne/scheduler/master/public/alex-reyne-scheduler.png', 
+'https://github.com/Alex-Reyne/scheduler', 'https://github.com/Alex-Reyne/scheduler', 'Original Request test');
+
+INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
+VALUES (2, 'Tinyapp',
+'A simple URL shortener app that takes a regular URL and transforms it into an encoded version (similar to bit.ly).', 
+'https://raw.githubusercontent.com/Alex-Reyne/tinyapp/master/docs/home.png', 
+'https://github.com/Alex-Reyne/tinyapp', 'https://github.com/Alex-Reyne/tinyapp', 'Original Request test');
+
+
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -13,13 +13,12 @@ import {
 	CardActionArea,
 	CardActions,
 } from '@mui/material';
-import PortfolioCard from '../components/PortfolioCard';
-import PortfolioModal from '../components/PortfolioModal';
 import UserProfileHeader from '../components/UserProfileHeader';
 import UserProfileBio from '../components/UserProfileBio';
 import NewProjectPost from '../components/NewProjectPost';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
+import UserProjects from '../components/UserProjects';
 
 export default function Profile() {
 	const { currentUser } = useContext(UserContext);
@@ -43,6 +42,8 @@ export default function Profile() {
 
 	const { first_name, last_name, id } = currentUser;
 
+	const newProjectModal = <NewProjectPost />;
+
 	useEffect(() => {
 		if (id) {
 			const devUrl = '/api/devs/' + id;
@@ -62,72 +63,6 @@ export default function Profile() {
 
 	console.log('profile', profile);
 	console.log('profile.projects:', profile.projects);
-
-	const projectsArray = profile.projects;
-	const parsedProjects = projectsArray.map(project => {
-		const data = (
-			<PortfolioModal
-				key={'Project-modal-' + project.project_id}
-				{...project}
-			/>
-		);
-		return (
-			<Grid
-				item
-				xs={1}
-				sm={6}
-				md={3}
-				key={'Project-grid-' + project.project_id}
-			>
-				<Grid
-					container
-					direction='column'
-					key={'Project-grid-container-' + project.project_id}
-				>
-					<Card key={'Project-card-' + project.project_id}>
-						<CardActionArea
-							key={'Job-card-action' + project.project_id}
-							onClick={() => {
-								setModalData(data);
-								handleView();
-							}}
-						>
-							<PortfolioCard
-								key={'Portfolio-card-' + project.project_id}
-								{...project}
-							/>
-						</CardActionArea>
-						<CardActions key={'Job-card-actions-' + project.project_id}>
-							<Button
-								key={'Job-button-github-' + project.project_id}
-								onClick={() =>
-									project.github_link
-										? window.open(project.github_link, '_self')
-										: null
-								}
-								disabled={!project.github_link}
-							>
-								Github
-							</Button>
-							<Button
-								key={'Job-button-live-' + project.project_id}
-								onClick={() =>
-									project.live_link
-										? window.open(project.live_link, '_self')
-										: null
-								}
-								disabled={!project.live_link}
-							>
-								Live Link
-							</Button>
-						</CardActions>
-					</Card>
-				</Grid>
-			</Grid>
-		);
-	});
-
-	const newProjectModal = <NewProjectPost />;
 
 	return (
 		<>
@@ -154,10 +89,16 @@ export default function Profile() {
 						Hey
 					</Button>
 					<Grid container>
-						<Grid item xs={12} id='profile-title'>
-							<h1>{`${first_name} ${last_name}`} Portfolio:</h1>
-						</Grid>
-						{parsedProjects}
+						<UserProjects
+							profile={profile}
+							setProfile={setProfile}
+							openModal={openModal}
+							setOpenModal={setOpenModal}
+							modalData={modalData}
+							setModalData={setModalData}
+							handleView={handleView}
+							projects={profile.projects}
+						/>
 					</Grid>
 				</section>
 				<Dialog

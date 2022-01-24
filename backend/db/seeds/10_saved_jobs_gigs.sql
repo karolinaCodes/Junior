11dd5826b9b3a3a0a9f0cf48563ca3be1642f55c
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-01-24 16:26:40 -0500

    removed some saved jobs seeds

--- backend/db/seeds/10_saved_jobs_gigs.sql
+++ backend/db/seeds/10_saved_jobs_gigs.sql
@@ -1,3 +1,2 @@
-INSERT INTO saved_jobs (junior_dev_id, job_posting_id) VALUES (1, 1);
 INSERT INTO saved_jobs (junior_dev_id, job_posting_id) VALUES (1, 2);
 INSERT INTO saved_jobs (junior_dev_id, job_posting_id) VALUES (1, 3);
--- backend/db/seeds/11_saved_gigs.sql
+++ backend/db/seeds/11_saved_gigs.sql
@@ -1,3 +1,1 @@
-INSERT INTO saved_gigs (junior_dev_id, gig_posting_id) VALUES (1, 1);
 INSERT INTO saved_gigs (junior_dev_id, gig_posting_id) VALUES (1, 2);
-INSERT INTO saved_gigs (junior_dev_id, gig_posting_id) VALUES (1, 3);

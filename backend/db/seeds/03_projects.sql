3c90183e3e3d2ba44d00cfa4a7f6eba2d68d502d
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-01-25 14:07:54 -0500

    add correct projects for larry user

--- backend/db/seeds/03_projects.sql
+++ backend/db/seeds/03_projects.sql
@@ -1,42 +1,26 @@
 INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
+VALUES (1, 'Junior',
+'Junior is a React app built with Express and is a combination of Indeed, Adobe Portfolio, and Fiver geared towards Junior Developers with under 3 years experience. Junior helps budding developers to showcase and build their portfolios. Connect with employers and keep active by completing gigs while looking for junior developer roles.', 
+'https://media.discordapp.net/attachments/374983262248108032/935436670713610300/unknown.png?width=851&height=422', 
+'https://github.com/Alex-Reyne/junior','http://localhost:3000/', 'Original Request test');
+
+INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, original_request)
+VALUES (1, 'Tweeter',
+'Tweeter is a simple, single-page Twitter clone that runs on your local system. I hope you are eggs-sighted to start Tweeting!', 
+'https://raw.githubusercontent.com/sarahdeecee/tweeter/master/docs/screenshot-big.png', 
+'https://github.com/sarahdeecee/tweeter', 'Create a single-page Twitter clone that updates using jQuery.');
+
+INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
 VALUES (1, 'Mocha and Chai',
 'A food & drink ordering app for a fictitious restaurant cafe called Mocha & Chai (kind of like the two well-known tools for unit testing). Our app uses Twilio to notify customers through SMS of accepted/ rejected orders and the restaurant of submitted orders.', 
 'https://user-images.githubusercontent.com/82968631/150898844-6590b5ce-39f3-4985-8024-7af02f277357.jpeg', 
 'https://github.com/karolinaCodes/Mocha-and-Chai/blob/master/README.md', 'https://github.com/karolinaCodes/Mocha-and-Chai/blob/master/README.md', 'Original Request test');
 
 INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
-VALUES (1, 'Interview Scheduler',
-'Interview Scheduler is a simple single-page application that allows students of the LightHouse Labs Web Development Bootcamp to book and manage an interview with a mentor. Interview Scheduler is built using React in the front-end and Express on the back-end. The data is served from the scheduler-api using a PostgreSQL database. Additionally, a suite of testing was built for this project to implement a deeper understanding of Jest and to begin working with the end-to-end testing framework, Cypress.
-
-', 
-'https://user-images.githubusercontent.com/82968631/150899382-9ae832c1-9f03-4de6-82fd-ba468c3c00e8.jpeg', 
-'https://github.com/karolinaCodes/Interview-Scheduler', 'https://github.com/karolinaCodes/Interview-Scheduler', 'Original Request test');
-
-INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
-VALUES (1, 'Jungle',
-'An e-commerce application built with Rails 4.2 for purposes of teaching Rails by example, by Lighthouse Labs. I added features, fixed bugs and upgraded the user-interface design of this existing web application in order to learn the Rails framework.', 
-'https://github.com/karolinaCodes/jungle-rails/raw/master/public/screenshots/upgraded-ui.jpeg', 
-'https://github.com/karolinaCodes/jungle-rails', 'https://github.com/karolinaCodes/Tinyapp', 'Original Request test');
-
-INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
-VALUES (1, 'ReactMeals',
-'ReactMeals is a single-page food ordering and delivery app with a firebase back-end.
-ReactMeals was built in the React- The Complete Guide to practice React components, the useContext Hook, and making HTTP requests in React, and in the end practice integrating a Firebase back-end.', 
-'https://user-images.githubusercontent.com/82968631/148492384-bd4c0259-11ca-47da-b86a-04eb077fe10b.png', 
-'https://github.com/karolinaCodes/ReactMeals', 'https://github.com/karolinaCodes/ReactMeals', 'Original Request test');
-
-INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
-VALUES (1, 'Forkify',
-'Forkify is a single-page food ordering and delivery app with a firebase back-end.
-Built in the React- The Complete Guide to practice React components, the useContext Hook, and making HTTP requests in React, and in the end practice integrating a Firebase back-end.', 
-'https://user-images.githubusercontent.com/82968631/148464876-055d7b17-38ad-4227-8818-559db270e6f6.png', 
-'https://github.com/karolinaCodes/Forkify', 'https://github.com/karolinaCodes/Forkify', 'Original Request test');
-
-INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
-VALUES (1, 'Tinyapp',
-'TinyApp is a simple URL shortener app that takes a regular URL and transforms it into an encoded version (similar to bit.ly). Built with Node and Express for the back end, and EJS and Bootstrap for the front-end.', 
-'https://github.com/karolinaCodes/Tinyapp/raw/master/docs/urls_index.jpeg', 
-'https://github.com/karolinaCodes/Tinyapp', 'https://github.com/karolinaCodes/Tinyapp', 'Original Request test');
+VALUES (1, 'Creative Reyne',
+'Creative Reyne is my design portfolio. Designed by myself and build by my friend before learned how to code and write web applications', 
+'https://raw.githubusercontent.com/Alex-Reyne/CreativeReyne/master/unknown.png', 
+'https://github.com/Alex-Reyne/CreativeReyne', 'https://www.creativereyne.ca/', 'Original Request test');
 
 INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
 VALUES (2, 'Mocha and Chai',
@@ -46,9 +30,7 @@ VALUES (2, 'Mocha and Chai',
 
 INSERT INTO projects (junior_dev_id, title, description, thumbnail_photo_url, github_link, live_link, original_request)
 VALUES (2, 'Interview Scheduler',
-'Interview Scheduler is a simple single-page application that allows students of the LightHouse Labs Web Development Bootcamp to book and manage an interview with a mentor. Interview Scheduler is built using React in the front-end and Express on the back-end. The data is served from the scheduler-api using a PostgreSQL database. Additionally, a suite of testing was built for this project to implement a deeper understanding of Jest and to begin working with the end-to-end testing framework, Cypress.
-
-', 
+'Interview Scheduler is a simple single-page application that allows students of the LightHouse Labs Web Development Bootcamp to book and manage an interview with a mentor. Interview Scheduler is built using React in the front-end and Express on the back-end. The data is served from the scheduler-api using a PostgreSQL database. Additionally, a suite of testing was built for this project to implement a deeper understanding of Jest and to begin working with the end-to-end testing framework, Cypress.', 
 'https://user-images.githubusercontent.com/82968631/150899382-9ae832c1-9f03-4de6-82fd-ba468c3c00e8.jpeg', 
 'https://github.com/karolinaCodes/Interview-Scheduler', 'https://github.com/karolinaCodes/Interview-Scheduler', 'Original Request test');
 
@@ -131,5 +113,3 @@ VALUES (4, 'Todai Food-Pickup Ordering',
 The restaurant can keep track of orders and enter how long it takes to prepare each order.', 
 'https://i.imgur.com/ZX9mQDv.jpeg', 
 'https://github.com/sarahdeecee/food-pickup', 'Create an app in a group of three using the skills you have so far.');
-
-

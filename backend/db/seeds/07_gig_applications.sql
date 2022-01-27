162e5d361ca3a404e22612c55e743a1d48c6239d
Alex-Reyne <creativereyne@gmail.com>
2022-01-27 12:10:19 -0600

    fix seeds in db

--- backend/db/seeds/07_gig_applications.sql
+++ backend/db/seeds/07_gig_applications.sql
@@ -1,8 +1,8 @@
 INSERT INTO gig_applications (gig_posting_id, junior_dev_id, date_applied, is_accepted)
-VALUES (2, 1, '2022-01-18T21:05:44.519Z',false);
+VALUES (5, 1, '2022-01-18T21:05:44.519Z',false);
 INSERT INTO gig_applications (gig_posting_id, junior_dev_id, date_applied, is_accepted)
-VALUES (5, 2, '2022-01-19T21:05:44.519Z',false);
+VALUES (5, 4, '2022-01-19T21:05:44.519Z',false);
 INSERT INTO gig_applications (gig_posting_id, junior_dev_id, date_applied, is_accepted)
 VALUES (5, 3, '2022-01-20T21:05:44.519Z',false);
 INSERT INTO gig_applications (gig_posting_id, junior_dev_id, date_applied, is_accepted)
-VALUES (5, 4, '2022-01-19T21:05:44.519Z',false);
+VALUES (5, 2, '2022-01-19T21:05:44.519Z',false);

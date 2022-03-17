b10ab512e3afcf44ad744776da509e64b1e7d000
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-03-17 12:18:47 -0400

    add .gitignore

--- backend/db/reset.js
+++ backend/db/reset.js
@@ -1,10 +1,10 @@
 // load .env data into process.env
-require("dotenv").config();
+require('dotenv').config();
 
 // other dependencies
-const fs = require("fs");
-const chalk = require("chalk");
-const Client = require("pg-native");
+const fs = require('fs');
+const chalk = require('chalk');
+const Client = require('pg-native');
 
 // PG connection setup
 const connectionString =
@@ -15,10 +15,10 @@ const client = new Client();
 // Loads the schema files from db/schema
 const runSchemaFiles = function () {
   console.log(chalk.cyan(`-> Loading Schema Files ...`));
-  const schemaFilenames = fs.readdirSync("./db/schema");
+  const schemaFilenames = fs.readdirSync('./db/schema');
 
   for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./db/schema/${fn}`, "utf8");
+    const sql = fs.readFileSync(`./db/schema/${fn}`, 'utf8');
     console.log(`\t-> Running ${chalk.green(fn)}`);
     client.querySync(sql);
   }
@@ -26,10 +26,10 @@ const runSchemaFiles = function () {
 
 const runSeedFiles = function () {
   console.log(chalk.cyan(`-> Loading Seeds ...`));
-  const schemaFilenames = fs.readdirSync("./db/seeds");
+  const schemaFilenames = fs.readdirSync('./db/seeds');
 
   for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./db/seeds/${fn}`, "utf8");
+    const sql = fs.readFileSync(`./db/seeds/${fn}`, 'utf8');
     console.log(`\t-> Running ${chalk.green(fn)}`);
     client.querySync(sql);
   }
